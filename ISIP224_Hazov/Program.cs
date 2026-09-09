98ff76bc4831983b435d6c95a66f8a3bbb46f038
243466 <243466@KB37-1-303-C09>
2026-09-09 11:35:46 +0300

    Сделал ввод и запись трат

--- ISIP224_Hazov/Program.cs
+++ ISIP224_Hazov/Program.cs
@@ -14,7 +14,7 @@ namespace ISIP224_Hazov
             bool invalidecount = false;
             do
             {
-                Console.Write("Введите количество операций, которые хотите ввести (от 2 до 40)");
+                Console.Write("Введите количество операций, которые хотите ввести (от 2 до 40): ");
                 if (int.TryParse(Console.ReadLine(), out operation) && operation >= 2 && operation <= 40)
                 {
                     invalidecount = true;
@@ -24,6 +24,37 @@ namespace ISIP224_Hazov
                     Console.WriteLine("Ошибка! Введите корректное значение!");
                 }
             } while (!invalidecount);
+
+            string[] titles = new string[operation];
+            double[] amounts = new double[operation];
+
+            Console.WriteLine("Ввод трат по шаблону (Название услуги или товара; Количество денег) (Валюта - рубли)");
+            Console.WriteLine("Пример: Влажные салфетки (Лента); 235");
+            for (int i = 0; i < operation; i++)
+            {
+                bool isvalideExpense = false;
+                do
+                {
+                    Console.Write($"Операция {i + 1} из {operation}: ");
+                    string input = Console.ReadLine();
+                    string[] parts = input.Split(new char[] { ';' });
+                    if (parts.Length == 2)
+                    {
+                        string titlePart = parts[0].Trim();
+                        string amountPart = parts[1].Trim();
+                        if (!string.IsNullOrWhiteSpace(titlePart) && double.TryParse(amountPart, out double amount) && amount >= 0)
+                        {
+                            titles[i] = titlePart;
+                            amounts[i] = amount;
+                            isvalideExpense = true;
+                        }
+                    }
+                    if (!isvalideExpense)
+                    {
+                        Console.WriteLine("Ошибка! Используйте формат: Название; Сумма (сумма - положительное число)");
+                    }
+                } while (!isvalideExpense);
+            }
         }
     }
 }
