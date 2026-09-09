cb6391452ca4259992729b6b315ec0efd01c1988
243466 <243466@KB37-1-303-C09>
2026-09-09 10:44:01 +0300

    Добавил ввод количества операций, которые будут записаны

--- ISIP224_Hazov/Program.cs
+++ ISIP224_Hazov/Program.cs
@@ -10,6 +10,20 @@ namespace ISIP224_Hazov
     {
         static void Main(string[] args)
         {
+            int operation = 0;
+            bool invalidecount = false;
+            do
+            {
+                Console.Write("Введите количество операций, которые хотите ввести (от 2 до 40)");
+                if (int.TryParse(Console.ReadLine(), out operation) && operation >= 2 && operation <= 40)
+                {
+                    invalidecount = true;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Введите корректное значение!");
+                }
+            } while (!invalidecount);
         }
     }
 }
