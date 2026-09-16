00c75c29a5eee4549a5e8b790b8d00cc959b247b
243466 <243466@KB37-1-303-C09>
2026-09-16 10:36:02 +0300

    Сделал публичный класс Product

--- ISIP224_Hazov/Program.cs
+++ ISIP224_Hazov/Program.cs
@@ -8,6 +8,32 @@ namespace ISIP224_Hazov
 {
     internal class Program
     {
+        public enum Category
+        {
+            Electronics,
+            Groceries,
+            Clothing,
+            Household
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+            public Category Category { get; set; }
+            public bool IsInStock => Quantity > 0;
+
+            public Product(int id, string name, decimal price, int quantity, Category category)
+            {
+                Id = id;
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+                Category = category;
+            }
+        }
         static void Main(string[] args)
         {
         }
